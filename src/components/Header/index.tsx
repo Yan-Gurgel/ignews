fe86a8bdacae78a3f2938d696a14cbcf3dce7e19
Yan-Gurgel <yan.gurgel@gmail.com>
2021-04-09 09:59:43 -0300

    Add Componente Header

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,15 @@
+import styles from './styles.module.scss';
+//Header importado para o _app pois ele vai aparecer em todas as páginas
+export function Header() {
+    return (
+        <header className={styles.headerContainer}>
+            <div className={styles.headerContent}>
+                <img src="/images/logo.svg" alt="ig.news" />
+                <nav>
+                    <a className={styles.active}>Home</a>
+                    <a>Post</a>
+                </nav>
+            </div>
+        </header>
+    );
+}
