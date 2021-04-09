5a091dbad019b14cee4f9586eb8678741ce8ad06
Yan-Gurgel <yan.gurgel@gmail.com>
2021-04-09 11:22:01 -0300

    Add Componente SubscribeButton

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,7 @@
 import Head from 'next/head'
 
+import { SubscribeButton } from '../components/SubscribeButton'
+
 import styles from './home.module.scss'
 
 export default function Home() {
@@ -11,15 +13,16 @@ export default function Home() {
 
       <main className={styles.contentContainer}>
         <section className={styles.hero}>
-        <span>👏Hey, welcome</span>
-        <h1>News about the <span>React </span>world.</h1>
-        <p>
-          Get acess to all the publications <br/>
-          <span>for $9.90 moth</span>
-        </p>
+          <span>👏Hey, welcome</span>
+          <h1>News about the <span>React </span>world.</h1>
+          <p>
+            Get acess to all the publications <br />
+            <span>for $9.90 moth</span>
+          </p>
+          <SubscribeButton />
         </section>
 
-        <img src="/images/avatar.svg" alt="Girl coding"/>
+        <img src="/images/avatar.svg" alt="Girl coding" />
       </main>
     </>
   )
