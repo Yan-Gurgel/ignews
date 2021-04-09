c83ed79d909cd41c1fa0302c5f985c7c7725c244
Yan-Gurgel <yan.gurgel@gmail.com>
2021-04-09 10:42:52 -0300

    Add Componente SignInButton

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,3 +1,5 @@
+import { SignInButton } from '../SignInButton';
+
 import styles from './styles.module.scss';
 //Header importado para o _app pois ele vai aparecer em todas as páginas
 export function Header() {
@@ -9,6 +11,8 @@ export function Header() {
                     <a className={styles.active}>Home</a>
                     <a>Post</a>
                 </nav>
+
+                <SignInButton />
             </div>
         </header>
     );
